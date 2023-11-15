8baa79ab318163dc0d8b199a9b8defec6316c7e0
VinceMP <vincemolletplumel@gmail.com>
2023-11-15 15:34:50 +0100

    Premier commit

---
+++ CV/app.js
@@ -0,0 +1,16 @@
+function red() {
+  document.getElementById('title').style.color = "red";
+}
+
+function blue() {
+  document.getElementById('title').style.color = "blue";
+}
+
+function white() {
+  document.getElementById('title').style.color = "white";
+}
+
+function grey() {
+  document.getElementById('title').style.color = "grey";
+}
+
